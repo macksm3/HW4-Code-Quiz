5cb727872c0b9dd4a5f4238fb43a6fd4d5cb621c
MacksM3 <64023042+macksm3@users.noreply.github.com>
2020-07-14 16:15:12 -0400

    init files

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// Code Quiz 
+
+var startButton = document.querySelector("#btnStart");
+let gameOn = false
+
+startButton.addEventListener("click", function(event) {
+  event.preventDefault();
+  
+  // create user object from submission
+  gameOn = true;
+  console.log(gameOn);
+
+  });
+
+  
+
+  
